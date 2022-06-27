c2637b6b08e198343e3aeea75439a66a7baf26bb
Alfonso Gonzalez <271396+a13z@users.noreply.github.com>
2022-06-27 18:32:22 +0200

    Add twitter oath 1 legacy and add types

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,14 +5,18 @@ import TwitterProvider from "next-auth/providers/twitter"
 export const authOptions: NextAuthOptions = {
     providers: [
       MediumProvider({
-        clientId: process.env.MEDIUM_CLIENT_ID,
-        clientSecret: process.env.MEDIUM_CLIENT_SECRET,
+        clientId: process.env.MEDIUM_CLIENT_ID as string,
+        clientSecret: process.env.MEDIUM_CLIENT_SECRET as string,
         // checks: "state",
       }),
       TwitterProvider({
-        clientId: process.env.TWITTER_CLIENT_ID,
-        clientSecret: process.env.TWITTER_CLIENT_SECRET,
-        // version: "2.0", //
+        clientId: process.env.TWITTER_ID as string,
+        clientSecret: process.env.TWITTER_SECRET as string,
+      }),
+      TwitterProvider({
+        clientId: process.env.TWITTER_CLIENT_ID as string,
+        clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+        version: "2.0", //
       }),
       ],
       // callbacks: {
