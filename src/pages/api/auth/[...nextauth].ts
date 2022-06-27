c239d342a3a68f57a5043c7c99342e72fe5a72c7
Alfonso Gonzalez <271396+a13z@users.noreply.github.com>
2022-06-27 17:00:24 +0200

    Remove callbacks from next auth and add debugging to trace medium issue

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,24 +6,26 @@ export const authOptions: NextAuthOptions = {
     providers: [
       MediumProvider({
         clientId: process.env.MEDIUM_CLIENT_ID,
-        clientSecret: process.env.MEDIUM_CLIENT_SECRET
+        clientSecret: process.env.MEDIUM_CLIENT_SECRET,
+        checks: console.log(),
       }),
         // ...add more providers here
       ],
-      callbacks: {
-        async jwt({ token, account }) {
-            // Persist the OAuth access_token to the token right after signin
-            if (account) {
-            token.accessToken = account.access_token
-            }
-            return token
-        },
-        async session({ session, token, user }) {
-            // Send properties to the client, like an access_token from a provider.
-            session.accessToken = token.accessToken
-            return session
-        }
+      // callbacks: {
+      //   async jwt({ token, account }) {
+      //       // Persist the OAuth access_token to the token right after signin
+      //       if (account) {
+      //       token.accessToken = account.access_token
+      //       }
+      //       return token
+      //   },
+      //   async session({ session, token, user }) {
+      //       // Send properties to the client, like an access_token from a provider.
+      //       session.accessToken = token.accessToken
+      //       return session
+      //   }
       },
+      debug: true,
 }
 // export default NextAuth({
 //   // Configure one or more authentication providers
